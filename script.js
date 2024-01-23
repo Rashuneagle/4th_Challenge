1f52e0fe6b44951a05fa0f815ac1b808d7c7d133
Michael Scott <mscotteagle21@gmail.com>
2024-01-23 06:53:12 -0600

    added setInterval to code for the clock, and added a for loop to iterate through the questions

--- script.js
+++ script.js
@@ -1,5 +1,51 @@
 var startGameBtn = document.querySelector("#startGame")
+var timeLimit = document.getElementById("timeLeft")
 
 console.log(startGameBtn)
 
 startGameBtn.addEventListener("click", gameStart())
+
+var secondsLeft = 300;
+function gameStart(){
+
+    var questions[
+        {
+            question: "What is an eventListener?",
+            options:['op1', 'op2', 'op3', 'op4'],
+            correct: 'op1'
+        },
+        {
+            question:"question 2",
+            options:["op1", "op2", "op3", "op4"],
+            correct: "op2"
+        },
+        {
+            {
+                question:"question 2",
+                options:["op1", "op2", "op3", "op4"],
+                correct: "op2"
+            },
+        }
+    ];
+
+    for(i=0; i<questions.length; i++){
+        
+    }
+
+
+    var timeInterval = setInterval(function () {
+        secondsLeft--;
+        timeLimit.textContent = secondsLeft;
+
+        if (secondsLeft <= 0) {
+            clearInterval(timeInterval);
+            results();
+        }
+    }, 1000);
+
+}
+
+function results(){
+
+
+}
